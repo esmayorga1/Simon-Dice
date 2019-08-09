f32ef5dfdd0cfb09f96d658416c636a9a24b6251
Andres Santana <drew.lizcano@gmail.com>
2019-08-09 15:31:04 -0500

    empezando mascador

---
+++ simonDice.js
@@ -0,0 +1,10 @@
+
+const puntos = document.getElementById('puntos')
+
+
+// var PuntosTotales = prompt('puntos?')
+console.log(puntos)
+puntos.innerHTML = PuntosTotales
+function empezarJuego(){
+  swal('hola')
+}
