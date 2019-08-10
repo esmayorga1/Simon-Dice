678fbb5868c2908c988974a6270793b723e35941
Andres Santana <drew.lizcano@gmail.com>
2019-08-09 21:41:40 -0500

    agregando rotacion

--- simonDice.js
+++ simonDice.js
@@ -4,24 +4,33 @@ const celeste = document.getElementById('celeste')
 const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
+const gameBoard = document.getElementById('gameboard')
+const btn_empezar = document.getElementById('btn_empezar')
+// const gradosGiro = 0
+btn_empezar.addEventListener('click',empezarJuego)
+const ULTIMO_NIVEL = 10
 class Juego{
   constructor(){
     this.inicializar = this.inicializar.bind(this)
+    this.giro = 0
     this.inicializar()
     this.generarSecuencia()
     setTimeout( ()=>{
       this.siguienteNivel()
-    },1000)
+    },2000)
   }
   inicializar(){
-    // this.elegirColor = this.elegirColor.bind(this)
+    this.elegirColor = this.elegirColor.bind(this)
+    this.siguienteNivel = this.siguienteNivel.bind(this)
+    this.toggleBtnEmpezar()
     this.nivel = 1
+    this.puntosJuego = 0
     this.colores = {
       celeste,violeta,naranja,verde
     }
-    this.toggleBtnEmpezar()
   }
   toggleBtnEmpezar(){
+    // debugger
     if(btn_empezar.classList.contains('hide')){
       btn_empezar.classList.remove('hide')
     }else{
@@ -31,10 +40,13 @@ class Juego{
   generarSecuencia(){
     this.secuencia = new Array(10).fill(0).map(n=>Math.floor(Math.random()*4))
   }
+  
   siguienteNivel(){
+    // this.iluminarSecuencia = this.iluminarSecuencia.bind(this)
     this.subnivel=0
     this.iluminarSecuencia()
     this.agregarEventosClick()
+    
   }
   numeroAcolor(numero){
     switch(numero){
@@ -50,7 +62,7 @@ class Juego{
 
   }
   colorAnumero(color){
-    switch(numero){
+    switch(color){
       case 'celeste' :
         return 0
       case 'violeta' : 
@@ -60,13 +72,85 @@ class Juego{
       case 'verde' : 
       return 3
     }
-
   }
+  iluminarSecuencia(){
+    for(let i = 0; i< this.nivel; i++){
+      let color = this.numeroAcolor(this.secuencia[i])
+      setTimeout( ()=> this.iluminarColor(color),1000*i)
+    }
+  }
+ 
+  iluminarColor(color){
+    this.colores[color].classList.add('light')
+    setTimeout( ()=> this.apagarColor(color),350)
+  }
+  apagarColor(color){
+    this.colores[color].classList.remove('light')
+  }
+  agregarEventosClick(){
+    this.colores.celeste.addEventListener('click',this.elegirColor)
+    this.colores.violeta.addEventListener('click',this.elegirColor)
+    this.colores.naranja.addEventListener('click',this.elegirColor)
+    this.colores.verde.addEventListener('click',this.elegirColor)
+  }
+  eliminarEventosClick(){
+    this.colores.celeste.removeEventListener('click',this.elegirColor)
+    this.colores.violeta.removeEventListener('click',this.elegirColor)
+    this.colores.naranja.removeEventListener('click',this.elegirColor)
+    this.colores.verde.removeEventListener('click',this.elegirColor)
+  }
+  elegirColor(ev){
+    const nombreColor = ev.target.dataset.color
+    const numeroColor = this.colorAnumero(nombreColor)
+    this.iluminarColor(nombreColor)
+
+    if(numeroColor === this.secuencia[this.subnivel]){
+      this.subnivel++
+      this.sumarPuntos()
+      if(this.subnivel === this.nivel){
+        this.nivel++
+        
+        this.giro++
+        this.rotar()
+        this.eliminarEventosClick()
+        if(this.nivel === ULTIMO_NIVEL + 1 ){
+          this.ganoElJuego()
+        }else{
+          setTimeout(this.siguienteNivel,1500)
+        }
+      }
+    }else{
+      this.perdioElJuego()
+    }
+  }
+  sumarPuntos(){
+    this.puntosJuego+=10
+    puntos.innerHTML = this.puntosJuego
+  }
+  ganoElJuego(){
+    swal('SimonDice','Ganaste','success')
+    .then( ()=>{
+      this.inicializar()
+     
+    })
+  }
+  perdioElJuego(){
+    swal('SimonDice','Buen intento', 'error')
+    .then( ()=>{
+      this.eliminarEventosClick()
+      this.inicializar()
+    })
+  }
+  rotar(){
+    var angulo = this.giro *45
+
+    var rotacion = `rotate(${angulo}deg)`
+    gameBoard.style.transform = rotacion 
+  }
+  
 }
 
 // var PuntosTotales = prompt('puntos?')
 function empezarJuego(){
   juego = new Juego()
 }
-const btn_empezar = document.getElementById('btn_empezar')
-btn_empezar.addEventListener('click',empezarJuego)
